65324a89d3b63d1ad897dc9016f3b8c2433d5ed2
Cole Moore <Cartisim@icloud.com>
2024-07-23 09:09:36 +0800

    Updated Versions

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-crypto.git", "2.6.0"..."3.3.0"),
-        .package(url: "git@github.com:needle-tail/swift-data-to-file.git", .upToNextMajor(from: "1.0.2")),
+        .package(url: "git@github.com:needle-tail/swift-data-to-file.git", .upToNextMajor(from: "1.0.5")),
         .package(url: "https://github.com/orlandos-nl/BSON.git", from: "8.1.0")
     ],
     targets: [
