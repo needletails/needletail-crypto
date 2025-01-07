a79af3adf48995b85f70a28a79f14e8c2dd69d31
Cole Moore <Cartisim@icloud.com>
2025-01-07 17:48:22 +0800

    Removed unneeded dependencies

--- Package.swift
+++ Package.swift
@@ -19,8 +19,6 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-crypto.git", .upToNextMajor(from: "3.7.1")),
         .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.1.3")),
         .package(url: "https://github.com/apple/swift-testing.git", .upToNextMajor(from: "0.10.0")),
-        .package(url: "git@github.com:needle-tail/needletail-helpers.git", .upToNextMajor(from: "1.0.4")),
-        .package(url: "https://github.com/needle-tail/needletail-algorithms.git", .upToNextMajor(from: "1.0.10")),
         .package(url: "https://github.com/orlandos-nl/BSON.git", from: "8.1.0"),
     ],
     targets: [
@@ -31,8 +29,6 @@ let package = Package(
             dependencies: [
                 .product(name: "Crypto", package: "swift-crypto"),
                 .product(name: "Collections", package: "swift-collections"),
-                .product(name: "NeedleTailHelpers", package: "needletail-helpers"),
-                .product(name: "NeedleTailAlgorithms", package: "needletail-algorithms"),
                 .product(name: "BSON", package: "BSON")
             ]
         ),
